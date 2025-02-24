c80409e050b2fa3d6b5eb4f75a192f19faec7790
dereje <getutadessedereje@icloud.com>
2025-02-24 11:55:53 +0100

    chore(config): add preact adapter for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,20 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import cloudflare from '@astrojs/cloudflare';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   adapter: cloudflare({
     imageService: 'cloudflare'
   }),
-  output: 'server'
+
+  output: 'server',
+  integrations: [preact({
+    compat: true
+  })]
 });
